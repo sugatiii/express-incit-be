6b60eefa2488a2ee9e2ad7a80827752538cb0826
ahmad.fahmi <ahmad.fahmi@62teknologi.com>
2024-08-08 12:30:51 +0700

    refactor config db change to env

--- utils/email.js
+++ utils/email.js
@@ -48,8 +48,7 @@ export const sendEmail = async (to, url, txt) => {
         html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
             <h2 style="text-align: center; text-transform: uppercase;color: teal;">Please Verify Your Email</h2>
-            <p>Congratulations! You're almost set to start using DEVAT✮SHOP.
-                Just click the button below to validate your email address.
+            <p>Congratulations!. Just click the button below to validate your email address.
             </p>
             
             <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
