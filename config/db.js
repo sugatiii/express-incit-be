8ccd248d0fb91d6d4cb00c3ccad0318c8db5e65e
ahmad.fahmi <ahmad.fahmi@62teknologi.com>
2024-08-08 11:19:55 +0700

    refactor config db change to env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,12 @@
 import {Sequelize} from "sequelize";
+import dotenv from 'dotenv';
 
-const db = new Sequelize('test_db','root','',{
-    host: 'localhost',
-    dialect: 'mysql'
+
+dotenv.config();
+
+const db = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USER,process.env.DATABASE_PASSWORD,{
+    host: process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT
 });
 
 export default db;
